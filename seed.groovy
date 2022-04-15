d117888c94a638caa5871f3c8a419bec37a39609
shuja <shuja.devops@gmail.com>
2022-04-15 15:19:51 +0530

    pipeline code

--- seed.groovy
+++ seed.groovy
@@ -39,3 +39,27 @@ pipelineJob('Roboshop Ansible') {
     }
   }
 }
+folder('CI-Pipelines') {
+  displayName('CI-Pipelines')
+  description('CI-Pipelines')
+}
+pipelineJob('CI-Pipelines/cart') {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://github.com/shuja-git/cart.git')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('Jenkinsfile')
+      'lightweight'(true)
+    }
+  }
+}
