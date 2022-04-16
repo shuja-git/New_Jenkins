ea934c0d7b49dd4fd278624710ae184622dc52f3
shuja <shuja.devops@gmail.com>
2022-04-16 12:36:29 +0530

    for loop used

---
+++ vars/golang.groovy
@@ -0,0 +1,34 @@
+def call() {
+
+    pipeline{
+        agent { label "${BUILD_LABEL}" }
+
+// triggers {  pollSCM('*/2 * * * *') }
+
+        stages {
+
+            stage('Compile Code'){
+                steps{
+                    sh 'echo Compile "${COMPONENT}" Code'
+                }
+            }
+
+            stage('Code Chek'){
+                steps{
+                    sh 'echo "${COMPONENT}" Code check'
+                }
+            }
+
+            stage('Test Cases'){
+                steps{
+                    sh 'echo "${COMPONENT}" Test case'
+                }
+            }
+
+
+        }
+    }
+
+
+}
+
