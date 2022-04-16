08143df9c55e6e501819ae7de3ffabb892d819bc
shuja <shuja.devops@gmail.com>
2022-04-16 11:42:50 +0530

    moved all pipeline

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,7 +1,42 @@
-def info(message) {
-    echo "INFO: ${message}"
+//def info(message) {
+//    echo "INFO: ${message}"
+//}
+//
+//def warning(message) {
+//    echo "WARNING: ${message}"
+//}
+//
+def call() {
+
+    pipeline{
+        agent { label 'WORKSTATION' }
+
+// triggers {  pollSCM('*/2 * * * *') }
+
+        stages {
+
+            stage('Compile Code'){
+                steps{
+                    sh 'echo Code check'
+                }
+            }
+
+            stage('Code Chek'){
+                steps{
+                    sh 'echo Code check'
+                }
+            }
+
+            stage('Test Cases'){
+                steps{
+                    sh 'echo Test case'
+                }
+            }
+
+
+        }
+    }
+
+
 }
 
-def warning(message) {
-    echo "WARNING: ${message}"
-}
