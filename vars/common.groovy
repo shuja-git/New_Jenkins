5ca94c46616744f06d15c6fc1700ef698d90ce68
shuja <shuja.devops@gmail.com>
2022-04-17 06:02:38 +0530

    login username and pwd

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,7 +1,7 @@
 def SonarQube(){
     println("SonarQube Testing- ")
 
-  sh 'sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.12.130:9000'
+  sh 'sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.12.130:9000 -Dsonar.login=admin -Dsonar.password=admin123'
 
 
 }
