1b6601f64dcffbcf8e941907b5ccabaab376eb27
shuja <shuja.devops@gmail.com>
2022-04-20 13:34:00 +0530

    if cond

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -14,7 +14,7 @@ def call() {
 // triggers {  pollSCM('H/2 * * * *') }
         environment {
             PROG_LANG_NAME = "nodejs"
-            PROG_LANG_VER  = "6"
+            PROG_LANG_VER  = "16"
             }
 
         stages {
